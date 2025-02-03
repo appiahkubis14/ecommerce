041dc7b09e68797292c002a74d1010636d613409
appiahkubis14 <appiahkubis14@gmail.com>
2025-02-03 16:18:41 +0000

    updated

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     
     "colorfield",
     # Apps
@@ -116,6 +117,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ecomm.urls'
--- ecomm/urls.py
+++ ecomm/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.http import HttpResponseServerError
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
@@ -15,8 +16,13 @@ urlpatterns = [
 
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    # urlpatterns += static(settings.STATICFILES_URL,
+    #                       view=lambda req: HttpResponseServerError('500: Server Error'))
+    # urlpatterns += static(settings.MEDIAFILES_URL,
+    #                       view=lambda req: HttpResponseServerError('500: Server Error'))
+    
 
 
 urlpatterns += staticfiles_urlpatterns()
